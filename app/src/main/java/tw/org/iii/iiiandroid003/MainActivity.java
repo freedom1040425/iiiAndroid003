d092e980aa3ee61102642b748ac681c3f3ce8176
Seraphim <freedom1040425@gmail.com>
2019-11-08 15:32:30 +0800

    life cycle

---
+++ app/src/main/java/tw/org/iii/iiiandroid003/MainActivity.java
@@ -0,0 +1,51 @@
+package tw.org.iii.iiiandroid003;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v("brad","onCreat");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("brad","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("brad","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("brad","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("brad","onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v("brad","onRestart");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("brad","onDestroy");
+    }
+}
